13e19e4d04f2d202ff5475e1a178bf90c5bf0447
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-03-29 01:02:08 +0700

    first commit

--- src/app/profile/changepassword/changepassword.component.ts
+++ src/app/profile/changepassword/changepassword.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../../main/header/header.component';
 import { ActivatedRoute, Router,RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { sha256 } from 'js-sha256'; // Import sha256 function from 'js-sha256' module
 
 @Component({
   selector: 'app-changepassword',
@@ -42,44 +43,47 @@ export class ChangepasswordComponent {
   New1: string = '';
   New2: string = '';
   passwordInput = '';
+  
 
-  Confirm(){
-    // console.log(this.Old, this.New1, this.New2);
-    const url = `https://adv-node.onrender.com/project/password/${this.userId}`;
-    this.http.get<any>(url).subscribe(
-      (data: any) => {
-        if(this.Old === data[0].user_password){
-          if(this.New1 !== this.New2){
-            alert('รหัสผ่านไม่ตรงกัน');
-          }
-          else{
-            const body = {
-              'user_password': this.New2
-            };
-      
-            const url = `https://adv-node.onrender.com/project/update/${this.userId}`;
-            this.http.put<any>(url, body).subscribe(
-              (data: any) => {
-                // console.log(data);
-                alert("เปลี่ยนรหัสผ่านสำเร็จ");
-                this.router.navigate(['/Profile', this.userId]); 
-              },
-              (error: any) => {
-                // console.log(error);
-                alert(error.error.message);
-              }
-            );
-          }
+  Confirm() {
+     // แปลงรหัสผ่านเป็นรูปแบบของ hash
+  const oldPasswordHash = sha256(this.Old);
+  const newPasswordHash = sha256(this.New2);
+
+  const url = `https://adv-node.onrender.com/project/password/${this.userId}`;
+  this.http.get<any>(url).subscribe(
+    (data: any) => {
+      if(oldPasswordHash === data[0].user_password){
+        if(this.New1 !== this.New2){
+          alert('รหัสผ่านไม่ตรงกัน');
         }
         else{
-          alert('รหัสผ่านไม่ถูกต้อง');
+          const body = {
+            'user_password': newPasswordHash
+          };
+    
+          const url = `https://adv-node.onrender.com/project/update/${this.userId}`;
+          this.http.put<any>(url, body).subscribe(
+            (data: any) => {
+              alert("เปลี่ยนรหัสผ่านสำเร็จ");
+              this.router.navigate(['/Profile', this.userId]); 
+            },
+            (error: any) => {
+              alert(error.error.message);
+            }
+          );
         }
-      },
-      (error: any) => {
-        console.log(error);
       }
-    );
+      else{
+        alert('รหัสผ่านไม่ถูกต้อง');
+      }
+    },
+    (error: any) => {
+      console.log(error);
+    }
+  );
   }
+  
 
   changeP(){
     alert('ll');
